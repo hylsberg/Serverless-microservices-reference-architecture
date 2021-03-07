149034b408ab7710b9079849c15c5f0d9eb1bae5
hylsberg <jonstormhylsberg@gmail.com>
2021-03-07 23:03:35 +0100

    settings.js

---
+++ web/serverless-microservices-web/public/js/setttings.js
@@ -0,0 +1,15 @@
+// Auth
+window.authClientId = '0017dc7c-ad26-419f-8bcc-84db8de1d27f';
+window.authAuthority = 'https://relecloudrideshare.b2clogin.com/tfp/relecloudrideshare.onmicrosoft.com/B2C_1_jhyrideshare/v2.0';
+window.authScopes = [
+'https://ridesharejhy.onmicrosoft.com/api/user_impersonation'
+];
+window.authEnabled = false;
+
+// API endpoints
+window.apiKey = 'a1ffdaef20054634ae37a7a74bbfdbb5';
+window.apiBaseUrl = 'https://ridesharejhy.azure-api.net';
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+window.signalrInfoUrl = 'https://ridesharetripsfunctionapp-jhy.azurewebsites.net/api/signalrinfo';

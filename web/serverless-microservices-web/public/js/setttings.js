71ee826127fc44892f78881a4858547c14277dde
hylsberg <jonstormhylsberg@gmail.com>
2021-03-08 15:15:44 +0100

    update window.authClientId after creating app reg

--- web/serverless-microservices-web/public/js/setttings.js
+++ web/serverless-microservices-web/public/js/setttings.js
@@ -1,5 +1,5 @@
 // Auth
-window.authClientId = '0017dc7c-ad26-419f-8bcc-84db8de1d27f';
+window.authClientId = '91317478-a887-4549-8229-96d472ad409f';
 window.authAuthority = 'https://relecloudrideshare.b2clogin.com/tfp/relecloudrideshare.onmicrosoft.com/B2C_1_jhyrideshare/v2.0';
 window.authScopes = [
 'https://ridesharejhy.onmicrosoft.com/api/rideshare'

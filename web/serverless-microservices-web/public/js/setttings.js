01c5d8d3b74b2b693d20ccf1c13dfa29528ae621
hylsberg <jonstormhylsberg@gmail.com>
2021-03-08 20:53:22 +0100

    Update setttings.js

--- web/serverless-microservices-web/public/js/setttings.js
+++ web/serverless-microservices-web/public/js/setttings.js
@@ -1,6 +1,6 @@
 // Auth
 window.authClientId = '0017dc7c-ad26-419f-8bcc-84db8de1d27f';
-window.authAuthority = 'https://ridesharejhy.b2clogin.com/tfp/ridesharejhy.onmicrosoft.com/b2c_1_jhyrideshare/v2.0';
+window.authAuthority = 'https://ridesharejhy.b2clogin.com/tfp/ridesharejhy.onmicrosoft.com/B2C_1_jhyrideshare/v2.0';
 window.authScopes = [
 'https://ridesharejhy.onmicrosoft.com/api/rideshare'
 ];
@@ -12,5 +12,5 @@ window.apiBaseUrl = 'https://ridesharejhy.azure-api.net';
 window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
 window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
 window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
-windows.apiOrchedtratorBaseUrl = 'https://ridehareorchestratorsfunctionapp-jhy.azurewebsites.net/api':
+windows.apiOrchedtratorBaseUrl = 'https://ridehareorchestratorsfunctionapp-jhy.azurewebsites.net/api';
 window.signalrInfoUrl = 'https://ridesharetripsfunctionapp-jhy.azurewebsites.net/api/signalrinfo';

2b47f816cfff44adbe2d7dd9a3a8937984efa593
hylsberg <jonstormhylsberg@gmail.com>
2021-03-08 01:45:45 +0100

    Update setttings.js

--- web/serverless-microservices-web/public/js/setttings.js
+++ web/serverless-microservices-web/public/js/setttings.js
@@ -4,7 +4,7 @@ window.authAuthority = 'https://relecloudrideshare.b2clogin.com/tfp/relecloudrid
 window.authScopes = [
 'https://ridesharejhy.onmicrosoft.com/api/rideshare'
 ];
-window.authEnabled = false;
+window.authEnabled = true;
 
 // API endpoints
 window.apiKey = 'a1ffdaef20054634ae37a7a74bbfdbb5';
@@ -12,4 +12,5 @@ window.apiBaseUrl = 'https://ridesharejhy.azure-api.net';
 window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
 window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
 window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+windows.apiOrchedtratorBaseUrl = 'https://ridehareorchestratorsfunctionapp-jhy.azurewebsites.net/api':
 window.signalrInfoUrl = 'https://ridesharetripsfunctionapp-jhy.azurewebsites.net/api/signalrinfo';

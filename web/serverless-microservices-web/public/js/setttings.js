0e0b3bee0f6c9ba73110a3834930ab10ed6d243e
hylsberg <jonstormhylsberg@gmail.com>
2021-03-08 02:18:32 +0100

    Update setttings.js

--- web/serverless-microservices-web/public/js/setttings.js
+++ web/serverless-microservices-web/public/js/setttings.js
@@ -1,6 +1,6 @@
 // Auth
 window.authClientId = '0017dc7c-ad26-419f-8bcc-84db8de1d27f';
-window.authAuthority = 'https://relecloudrideshare.b2clogin.com/tfp/relecloudrideshare.onmicrosoft.com/B2C_1_jhyrideshare/v2.0';
+window.authAuthority = 'https://ridesharejhy.b2clogin.com/ridesharejhy.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_jhyrideshare';
 window.authScopes = [
 'https://ridesharejhy.onmicrosoft.com/api/rideshare'
 ];
